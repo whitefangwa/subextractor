53e40b465aea0a1438558bc40f220d305b023f7f
mikiadarsh25 <57398892+mikiadarsh25@users.noreply.github.com>
2021-04-22 22:50:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @trojanzhex
+# @mikiadarsh25
 
 
 class Script(object):
@@ -13,7 +13,7 @@ Send me any video file and extract what you want :).
 
 See <i>help</i> for more details.
 
-© @TroJanzHEX</b>"""
+© @mikiadarsh25</b>"""
 
 
     HELP_MSG = """<b>Hai, Follow these Steps..</b>
@@ -26,12 +26,12 @@ See <i>help</i> for more details.
 
 🌀 <i>Now select the stream you need to extract!</i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @mikiadarsh25</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @mikiadarsh25    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
